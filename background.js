07a8c34c543a3225ee379ff66e246748dcc0ea7d
wen200796 <wen200796@gmail.com>
2025-02-02 23:19:23 +0800

    250202 23:19

--- background.js
+++ background.js
@@ -35,20 +35,37 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       const activeTab = tabs[0];
       const url = activeTab.url;
 
-      // 根據 URL 決定要查找的元素選擇器
+      // 根據 URL 決定要查找的元素選擇器和檢查邏輯
       var elementSelector;
+      var checkFunction;
       if (url.startsWith(ValidUrlPrefix.MAP)) {
         elementSelector = ".hh2c6[aria-label*='評論'][aria-selected='true']";
+        checkFunction = (selector) => {
+          return document.querySelector(selector) !== null;
+        };
+      } else if (url.startsWith(ValidUrlPrefix.SEARCH)) {
+        elementSelector = ".zzG8g[aria-selected='true']";
+        checkFunction = (selector) => {
+          const elements = document.querySelectorAll(selector);
+          for (const element of elements) {
+            const span = element.querySelector('span');
+            if (span && span.innerText.includes("評論")) {
+              return true;
+            }
+          }
+          return false;
+        };
       } else {
         elementSelector = "defaultSelector";
+        checkFunction = (selector) => {
+          return document.querySelector(selector) !== null;
+        };
       }
 
       // 在 activeTab 中執行腳本來檢查指定元素是否存在
       chrome.scripting.executeScript({
         target: { tabId: activeTab.id },
-        func: (selector) => {
-          return document.querySelector(selector) !== null;
-        },
+        func: checkFunction,
         args: [elementSelector]
       }, (results) => {
         if (results[0].result) {
